34c1a6468121d9bdaf5d6dc466534f255e3c1434
edxco <eduardo.baeza@yahoo.com>
2020-12-02 22:24:17 -0600

    Add login form and logout button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            flash[:notice] = "User was created succesfully"
+            flash[:notice] = "Welcome #{user.username}, to private events"
             redirect_to users_path
         else
             render 'new'
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :new, :create, :show, :destroy]
   resources :events, only: [:index, :new, :create, :show, :destroy]
   get 'signup', to: 'users#new'
+  resources :session, only: [:new, :create, :destroy]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
