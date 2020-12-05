e767ff57f85a00936878dfb968e515563cef134b
mke2111 <roymke134@gmail.com>
2020-12-05 20:26:16 +0300

    test use model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  user = User.new
+
+  it 'is valid when the name is present' do
+    user.name = 'User1'
+    expect(user).to be_valid
+  end
+
+  it 'is valid when name is present' do
+    user.name = nil
+    expect(user).to_not be_valid
+  end
 end
