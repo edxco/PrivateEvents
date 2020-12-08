fa9d7d6f43668d46d55e6167c567314f230db0af
edxco <eduardo.baeza@yahoo.com>
2020-12-07 22:46:11 -0600

    Fix rubocop

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,28 +1,21 @@
 module ApplicationHelper
+  def attendance_user(user, event)
+    return render inline: "<%= link_to 'Login to assist', login_path%>" unless logged_in?
 
-    def attendance_user(user, event)
-        
-        return render inline: "<%= link_to 'Login to assist', login_path%>" unless logged_in?
-        
-        if Attendee.where(user_id: user, event_id: event).any?
-            render "./events/unattendEvent"
-        else
-            render "./events/attendEvent"
-        end
+    if Attendee.where(user_id: user, event_id: event).any?
+      render './events/unattendEvent'
+    else
+      render './events/attendEvent'
     end
+  end
 
+  def attendance_show(event)
+    return render inline: "<%= link_to 'Login to assist', login_path%>" unless logged_in?
 
-    def attendance_show(event)
-        return render inline: "<%= link_to 'Login to assist', login_path%>" unless logged_in?
-        
-            
-        if Attendee.where(user_id: current_user[:id], event_id: event).any?
-            render inline: "Attend"
-        else
-            render inline: "UnAtttend"
-        end
-        
+    if Attendee.where(user_id: current_user[:id], event_id: event).any?
+      render inline: 'Attend'
+    else
+      render inline: 'UnAtttend'
     end
-
+  end
 end
-
