74b837efc5355b8d4a92ddcd8a4f5c1dfc506a17
mke2111 <roymke134@gmail.com>
2020-12-09 03:51:05 +0300

    fix rubocop error

--- spec/features/events_spec.rb
+++ spec/features/events_spec.rb
@@ -1,34 +1,9 @@
 require 'rails_helper'
 RSpec.describe 'Creating an event', type: :system do
-  # before do
-  #   visit root_url
-  #   click_button 'LOGIN'
-
-  #   expect(current_path).to eq(new_session_path)
-  #   page.fill_in placeholder: 'Create a new username', with: 'User 1'
-  #   click_button 'Sign Up'
-  # end
-
   describe 'new event page' do
     it 'Loads the right content' do
       visit new_event_path
       expect(page).to have_content('You must be logged in to perform that action')
     end
   end
-
-  # it 'valid inputs' do
-  #   visit new_event_path
-  #   page.fill_in placeholder: 'Event Title', with: 'Event 1'
-  #   click_button 'Create'
-  #   visit events_path
-  #   expect(page).to have_content('Event 1')
-  # end
-
-  # it 'invalid inputs' do
-  #   visit new_event_path
-  #   page.fill_in placeholder: 'Event Title', with: ''
-  #   click_button 'Create'
-  #   # visit events_path
-  #   expect(page).to have_content("Name can't be blank")
-  # end
 end
--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -7,20 +7,4 @@ RSpec.describe 'Create user', type: :system do
       expect(page).to have_content('LOGIN')
     end
   end
-
-  # it 'valid inputs' do
-  #   visit new_user_path
-  #   page.fill_in placeholder: 'Create a new username', with: 'User 1'
-  #   click_button 'Sign Up'
-  #   visit users_path
-  #   expect(page).to have_content('User 1')
-  # end
-
-  # it 'valid inputs' do
-  #   visit new_user_path
-  #   page.fill_in placeholder: 'Create a new username', with: ''
-  #   click_button 'Sign Up'
-  #   # visit users_path
-  #   expect(page).to have_content("Name can't be blank")
-  # end
 end
